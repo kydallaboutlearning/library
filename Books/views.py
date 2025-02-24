4c0129b794f936e84e096f4cb6ff0962a7465eac
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-24 14:40:42 +0100

    Chore(home): added the views for the homepage

--- Books/views.py
+++ Books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from  .models import Book
 
 # Create your views here.
+
+def HomeView(request):
+     books = Book.objects.all()
+     return render(request,'home/home.html',{'books':books})
+
