72b86e37ba9bd5dc85e5b562c279517dadadb419
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-24 14:36:36 +0100

    Chore(Project): created the books app then added models

---
+++ Books/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+     title = models.CharField( max_length = 250)
+     subtitle = models.CharField(max_length= 250)
+     author = models.CharField( max_length = 250)
+     isbn = models.Charfield( max_length = 13)
+
+     # returning the title when the models is called
+     def __str__(self):
+          return self.title
