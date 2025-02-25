0dbfd7c8c9e7cf151e4e91f5a7119cfe1c984e21
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-25 14:40:27 +0100

    Refactor: changed the logic of ggetting testdata

--- Books/tests.py
+++ Books/tests.py
@@ -1,12 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
 from .models import Book
+from utils.test_variable import *
 
-# setting up variables
-t_title = "testtitle",
-t_subtitle = "testSubtitle"
-t_author = "testauthor"
-t_isbn = "testisbn"
+
 
 # Create your tests here.
 class BookTest(TestCase):
