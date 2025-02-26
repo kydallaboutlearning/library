a6e017abe644cd506ee64a1519a56a6d6aa1e0ab
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-26 23:21:27 +0100

    Updated the manage.py to allow easy change of settings

--- manage.py
+++ manage.py
@@ -2,11 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from utils.setting_rel import *
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', current_settings_mode)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
