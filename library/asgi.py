a35607399b31cf918bf466db3802602c6f77a908
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-26 23:25:31 +0100

    updated settings for production

--- library/asgi.py
+++ library/asgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+from utils.setting_rel import *
+
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', current_settings_mode)
 
 application = get_asgi_application()
