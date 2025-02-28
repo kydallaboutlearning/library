8d4f49753dfe47ed3ad75a7e285eb18ea5313a81
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-28 20:25:55 +0100

    Update: created a views for todolist

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+'todo/views'
 
-# Create your views here.
+
+"""Importing requirements"""
+from django.shortcuts import render
+from api.serializers import TodoSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from .models import Todo
+
+
+"""Create your views here."""
+
+# CReating a list view
+class TodoList(APIView):
+     def get(self,request,pk):
+          max_objects = 20
+          todos = Todo.objects.all()[:max_objects]
+          data = TodoSerializer(todos,many = True)
+          return Respomse(data)
