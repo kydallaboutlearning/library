c8496e1167dd23424fbc2ee095c07aca2f85fc12
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-28 21:09:28 +0100

    Chore: Created a serializer for the todo models

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,12 @@
 """ Importing requirements for the serializer"""
 from rest_framework import serializers
 
+# importing the modelsz from books
 from Books.models import Book
+
+# importing models  from todo
+from todo.models import Todo 
+
 
 # Builiding the serializer
 class BookSerializer(serializers.ModelSerializer):
@@ -11,3 +16,9 @@ class BookSerializer(serializers.ModelSerializer):
      class Meta:
           model = Book
           fields = ('title','subtitle','author','isbn')
+
+
+class TodoSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Todo
+          fields = '__all__'
