2ef413d6430c2918976edfb85181e04d41006326
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-28 21:18:48 +0100

    Refactor: Changed the structure for the list view then added the details view

--- todo/views.py
+++ todo/views.py
@@ -5,7 +5,7 @@
 from django.shortcuts import render
 from api.serializers import TodoSerializer
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 from django.shortcuts import get_object_or_404
 from .models import Todo
 
@@ -13,9 +13,11 @@ from .models import Todo
 """Create your views here."""
 
 # CReating a list view
-class TodoList(APIView):
-     def get(self,request,pk):
-          max_objects = 20
-          todos = Todo.objects.all()[:max_objects]
-          data = TodoSerializer(todos,many = True)
-          return Respomse(data)
+class TodoList(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class TodoDetail(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
