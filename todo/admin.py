51680d98875f6aa0962785819fb0d6713e1aba3e
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-02-28 21:12:38 +0100

    Chore: Added a the todo to the admin section

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,14 @@
+# todo/views.py
+
+"""Importing requirements"""
 from django.contrib import admin
+from .models import Todo
 
 # Register your models here.
+admin.site.register(Todo)
+
+
+class registerAdmin(admin.ModelAdmin):
+     class Meta:
+          model = Todo
+     list_display = ['start_date','end_date','completed']
