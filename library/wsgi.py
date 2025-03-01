7bb1b88a130021d3d9faf7a6e8f4e161211efd9b
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-03-02 00:22:43 +0100

    Fix: added the corrects settings

--- library/wsgi.py
+++ library/wsgi.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
+from utils.setting_rel import *
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', current_settings_mode)
+
 
 application = get_wsgi_application()
