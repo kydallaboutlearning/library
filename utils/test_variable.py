f25ca0982d26ab9b343908deebfd10b5b269a9aa
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-03-02 21:27:34 +0100

    Chore: added more variable for testing purposes

--- utils/test_variable.py
+++ utils/test_variable.py
@@ -1,4 +1,16 @@
+# utils/test_variables.py
+"""Importinng Requiremensts"""
+from django.utils import timezone
+import datetime
+from django.conf import settings
+from accounts.models import CustomUser
+from django.contrib.auth import get_user_model
+
+
+
+
 """Creating reusable test variables """
+
 
 # setting up variables
 t_title = "testtitle",
@@ -6,3 +18,9 @@ t_subtitle = "testSubtitle"
 t_author = "testauthor"
 t_isbn = "testisbn"
 t_body = 'testbody'
+t_time = timezone.now()
+user = get_user_model()
+t_username = 'testusername'
+t_email = 'testemail@gmail.com'
+t_password = 'testpassoword'
+t_age = 19
