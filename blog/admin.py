64a955b33624ddbbb398f46e2538c3de4f325079
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-03-02 07:56:35 +0100

    Chore:Added an admin for the models post

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+     class Meta:
+          model = Post
+          fields = "__all__"
+
+     list_display = ['title', 'author','status', 'publish', 'created_at','updated_at']
+     list_editable = ['status']
+     search_fields = ['title', 'status', 'publish']  
+     ordering = ['status', 'publish']
+     show_facets = admin.ShowFacets.ALWAYS
+    
+     def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('title',)}
+
