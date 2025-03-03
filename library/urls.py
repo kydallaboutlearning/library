0d7e591c6755a15386dd461a669d3985429ce0ac
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-03-03 09:40:52 +0100

    Chore: Added the blog api to the project urls

--- library/urls.py
+++ library/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('',include('Books.urls')),
     path('api/',include('api.urls')),
     path('api/todo/', include('todo.urls')),
+    path('api/blog/')
     path('admin/', admin.site.urls),
 ]
