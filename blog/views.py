298efb23d09e76a9f78c4c276891d00c86598b1f
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-03-04 02:47:12 +0100

    Chore: Added the class based view for the bloglist and blogdetail

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from api.serializers import BlogApiSerializer
+from .models import Post
 
 # Create your views here.
+class BlogListView(generics.ListCreateAPIView):
+     queryset = Post.objects.all()
+     serializer_class = BlogApiSerializer
+
+class BlogDetailView(generics.RetrieveUpdateDestroyAPIView):
+     queryset = Post.objects.all()
+     serializer_class = BlogApiSerializer
