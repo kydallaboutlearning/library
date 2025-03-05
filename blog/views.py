c96bd7f7961abb1b8cd48da5a8a37d5cc854768a
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-03-06 00:06:01 +0100

    Chore: added permissions to the views

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import (
+                         generics,
+                         permissions
+                    )
 from api.serializers import BlogApiSerializer
 from .models import Post
+from api.permissions import *
 
 # Create your views here.
 class BlogListView(generics.ListCreateAPIView):
      queryset = Post.objects.all()
      serializer_class = BlogApiSerializer
+     permission_classes = (IsAdminUserorReadOnly,)
 
 class BlogDetailView(generics.RetrieveUpdateDestroyAPIView):
+     permission_class = permissions.IsAdminUser
      queryset = Post.objects.all()
+     permission_classes = (IsAdminUserorReadOnly,)
      serializer_class = BlogApiSerializer
