ec990b58aca366b9ebde482a88a5099f1625445b
kydallaboutlearning <156302772+kydallaboutlearning@users.noreply.github.com>
2025-03-07 20:53:44 +0100

    Update base.py
    Added authentication settings for the API 
    
    Signed-off-by: kydallaboutlearning <156302772+kydallaboutlearning@users.noreply.github.com>

--- library/settings/base.py
+++ library/settings/base.py
@@ -142,5 +142,8 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 REST_FRAMEWORK = {
      'DEFAULT_PERMISSION_CLASSES':
      ['rest_framework.permissions.IsAuthenticated'
-                                   ],}
+                                   ],
+'DEFAULT_AUTHENTICATION_CLASSES':
+['rest_framework.authentication.SessionAuthentication',
+ 'rest_framework.authentication.BasicAuthentication'}
 
