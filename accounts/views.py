a773a73f6b35348b55581888f2fbe0980f43a931
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-03-11 21:29:44 +0100

    Updated the views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
+# accounts/api.py
+
+"""Importing requirements for the views"""
+
 from django.shortcuts import render
 
-# Create your views here.
+# getting the serializers
+from api.serializers import UserSerializer
+
+# importing from the rst_framewor
+from rest_framework  import generics
+
+# importing the user model
+from django.contrib.auth import get_user_model
+
+"""# Create your views here."""
+
+# usserist api view
+class UserListAPi(generics.ListAPIView):
+     queryset = get_user_model
+     serializer_class = UserSerializer
+
+#user details models 
+class UserDetailAPi(generics.ListAPIView):
+     queryset = get_user_model
+     serializer_class = UserSerializer
