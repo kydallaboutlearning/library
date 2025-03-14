641244241530b280324b877ff4d454c811b1608e
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-03-14 19:42:40 +0100

    Chore: Added url for theuser api

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .views import UserListAPi, UserDetailAPi
 
 
-urlpatterns = []
+urlpatterns = [
+     path('',UserListAPi.as_view(),name ='users-list'),
+     path('<int:pk>/',UserDetailAPi.as_view(),name = 'user-details')
+]
